8acc62f642c02a90506745d4a7e466a2547a5e1f
Tiebe <tiebe.groosman@gmail.com>
2021-03-29 16:21:37 +0200

    Add Python detection

--- Program.cs
+++ Program.cs
@@ -5,6 +5,7 @@ using System.IO;
 using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
+using Microsoft.Win32;
 using Newtonsoft.Json;
 
 namespace SaveTheWorldRewards
@@ -119,12 +120,80 @@ namespace SaveTheWorldRewards
             }
 		}
 
+        private static string GetPythonPath(string requiredVersion = "", string maxVersion = "") {
+            string[] possiblePythonLocations = new string[3] {
+                @"HKLM\SOFTWARE\Python\PythonCore\",
+                @"HKCU\SOFTWARE\Python\PythonCore\",
+                @"HKLM\SOFTWARE\Wow6432Node\Python\PythonCore\"
+            };
+
+            //Version number, install path
+            Dictionary<string, string> pythonLocations = new Dictionary<string, string>();
+
+            foreach (string possibleLocation in possiblePythonLocations) {
+                string regKey = possibleLocation.Substring(0, 4), actualPath = possibleLocation.Substring(5);
+                RegistryKey theKey = (regKey == "HKLM" ? Registry.LocalMachine : Registry.CurrentUser);
+                RegistryKey theValue = theKey.OpenSubKey(actualPath);
+
+                foreach (var v in theValue.GetSubKeyNames()) {
+                    RegistryKey productKey = theValue.OpenSubKey(v);
+                    if (productKey != null) {
+                        try {
+                            string pythonExePath = productKey.OpenSubKey("InstallPath").GetValue("ExecutablePath").ToString();
+                            if (pythonExePath != null && pythonExePath != "") {
+                                //Console.WriteLine("Got python version; " + v + " at path; " + pythonExePath);
+                                pythonLocations.Add(v.ToString(), pythonExePath);
+                            }
+                        } catch {
+                            //Install path doesn't exist
+                        }
+                    }
+                }
+            }
+
+            if (pythonLocations.Count > 0) {
+                System.Version desiredVersion = new System.Version(requiredVersion == "" ? "0.0.1" : requiredVersion),
+                    maxPVersion = new System.Version(maxVersion == "" ? "999.999.999" : maxVersion);
+
+                string highestVersion = "", highestVersionPath = "";
+
+                foreach (KeyValuePair<string, string> pVersion in pythonLocations) {
+                    //TODO; if on 64-bit machine, prefer the 64 bit version over 32 and vice versa
+                    int index = pVersion.Key.IndexOf("-"); //For x-32 and x-64 in version numbers
+                    string formattedVersion = index > 0 ? pVersion.Key.Substring(0, index) : pVersion.Key;
+
+                    System.Version thisVersion = new System.Version(formattedVersion);
+                    int comparison = desiredVersion.CompareTo(thisVersion),
+                        maxComparison = maxPVersion.CompareTo(thisVersion);
+
+                    if (comparison <= 0) {
+                        //Version is greater or equal
+                        if (maxComparison >= 0) {
+                            desiredVersion = thisVersion;
+
+                            highestVersion = pVersion.Key;
+                            highestVersionPath = pVersion.Value;
+                        } else {
+                            //Console.WriteLine("Version is too high; " + maxComparison.ToString());
+                        }
+                    } else {
+                        //Console.WriteLine("Version (" + pVersion.Key + ") is not within the spectrum.");
+                    }
+                }
+
+                //Console.WriteLine(highestVersion);
+                //Console.WriteLine(highestVersionPath);
+                return highestVersionPath;
+            }
+
+            return "";
+        }
 		private static async Task RunPython(string url, string authCode)
         {
             // 1) Create Process Info
             var psi = new ProcessStartInfo
             {
-                FileName = @"C:\Program Files\Python\python.exe"
+                FileName = @GetPythonPath()
             };
 
             // 2) Provide script and arguments
